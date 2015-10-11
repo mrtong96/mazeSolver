f31f029cd3365d7493f43f9518ac07d6d59450c6
zheryu <eric.zhou.us@gmail.com>
2015-10-11 02:37:27 -0700

    first semblance of a server. Trying to make it better.

---
+++ server.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+var multer = require('multer');
+var multer = require('multer');
+var upload = multer({dest: 'uploads/'});
+var spawn = require('child_process').spawn;
+var py = spawn('python', ['dummy.py']);
+/*app.use(multer({ dest: './uploads/',
+    rename: function (fieldname, filename) {
+        return filename+Date.now();
+    },
+    onFileUploadStart: function (file) {
+        console.log(file.originalname + ' is starting ...');
+    },
+    onFileUploadComplete: function (file) {
+        console.log(file.fieldname + ' uploaded to  ' + file.path)
+    }
+}));*/
+//View Setup
+//app.use(express.static(path.join(rootPath + '/public'))); // public
+//app.use('/angular', express.static(path.join(rootPath + '/angular')));
+//app.set('view engine', 'ejs');
+// pyshit ======================================================================
+py.stdout.on('data', function(data){
+    console.log(data.toString());
+});
+
+py.stdin.write(JSON.stringify('lol'));
+py.stdin.end();
+
+// routes ======================================================================
+app.get('/',function(req,res){
+      res.sendFile(__dirname + "/index.html");
+});
+
+app.post('/api/photo', upload.single('maze'), function(req, res){
+    console.log(req.file);
+    console.log(req.files);
+    //console.log(req.body);
+    /*upload(req,res,function(err) {
+        if(err) {
+            return res.end("Error uploading file.");
+        }
+        res.end("File is uploaded");
+    });*/
+	res.end("File is uploaded");
+});
+
+app.listen(port, function(){
+	console.log('Listening at http://localhost:%s',port);
+});
